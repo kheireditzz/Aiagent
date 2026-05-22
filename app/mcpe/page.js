8fba96107ff6c7cf1ee61121711f21e175551fcb
Kheireditz <miftahulkhairim1@gmail.com>
2026-05-23 03:55:13 +0700

    Add MCPE server page

---
+++ app/mcpe/page.js
@@ -0,0 +1,3 @@
+import MCPEServerPanel from '../../components/MCPEServerPanel'
+
+export default function MCPEPage(){return <main className="page"><div className="wrap"><div className="nav"><b>MCPE Server</b><div className="links"><a className="link" href="/dashboard">Dashboard</a><a className="link" href="/settings">Settings</a></div></div><MCPEServerPanel/></div></main>}
