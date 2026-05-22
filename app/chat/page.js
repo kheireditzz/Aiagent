7b364dec5c78dbccb7f707382875c84a65740c5a
Kheireditz <miftahulkhairim1@gmail.com>
2026-05-23 03:26:59 +0700

    Add Gemini chat page

---
+++ app/chat/page.js
@@ -0,0 +1,3 @@
+import GeminiChat from '../../components/GeminiChat'
+
+export default function ChatPage(){return <main className="page"><div className="wrap"><div className="nav"><b>Aiagent Chat</b><div className="links"><a className="link" href="/dashboard">Dashboard</a><a className="link" href="/settings">Settings</a></div></div><GeminiChat/></div></main>}
