a3c205e84a0bcec2d9efdda8ba8f87db67bc4464
Kheireditz <miftahulkhairim1@gmail.com>
2026-05-23 03:52:27 +0700

    Add Cloudflare AI Search page

---
+++ app/cloudflare-search/page.js
@@ -0,0 +1,3 @@
+import CloudflareSearchPanel from '../../components/CloudflareSearchPanel'
+
+export default function CloudflareSearchPage(){return <main className="page"><div className="wrap"><div className="nav"><b>Cloudflare AI</b><div className="links"><a className="link" href="/dashboard">Dashboard</a><a className="link" href="/settings">Settings</a></div></div><CloudflareSearchPanel/></div></main>}
